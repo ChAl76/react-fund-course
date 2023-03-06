23ffd0d5449a69ea8696f3bbb09a1ce399e7eed7
ChAl76 <chornyalexander@gmail.com>
2023-03-06 18:57:52 +0200

    1. TWO-WAY BINDING
    App.js
    (Связывание состояния со значением, которое находится в инпуте. Достаем значение и помещаем его в состояние. ДВУСТОРОННЕЕ СВЯЗЫВАНИЕ)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [likes, setLikes] = useState(5);
+  const [value, setValue] = useState("text in input");
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        // Связывание состояния со значением, которое
+        // находится в инпуте (Достаем значение и помещаем его в состояние).
+        onChange={(event) => setValue(event.target.value)} // ДВУСТОРОННЕЕ СВЯЗЫВАНИЕ.
+      />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 export default App;
